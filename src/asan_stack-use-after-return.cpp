8c289b7c16ed1c6e8f3b6d0d211fea8deeced5da
Toxe <toxe@toxe.net>
2021-03-19 00:03:35 +0100

    Improve stack-use-after-return test

--- src/asan_stack-use-after-return.cpp
+++ src/asan_stack-use-after-return.cpp
@@ -1,13 +1,15 @@
 // need to run with ASAN_OPTIONS=detect_stack_use_after_return=true
 
-int* return_pointer_to_stack_value()
+int* p;
+
+void get_pointer_to_stack_value()
 {
-    int i = 0;
-    return &i;
+    int local = 0;
+    p = &local;
 }
 
 int main()
 {
-    int* p = return_pointer_to_stack_value();
+    get_pointer_to_stack_value();
     return *p;
 }

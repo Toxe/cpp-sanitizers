7be5ff58b25a7c0004e8e1dd3af34d7f9f7d585a
Toxe <toxe@toxe.net>
2021-03-18 23:32:43 +0100

    Prevent compiler from optimizing the allocations away

--- src/asan_memory-leak_malloc.cpp
+++ src/asan_memory-leak_malloc.cpp
@@ -1,9 +1,11 @@
 // on macOS need to run with ASAN_OPTIONS=detect_leaks=true
 
 #include <cstdlib>
+#include <iostream>
 
 int main()
 {
-    auto values = malloc(10 * sizeof(int));
+    int* values = static_cast<int*>(malloc(10 * sizeof(int)));
+    std::cout << "values[0] = " << values[0] << '\n';
     // free(values);
 }
--- src/asan_memory-leak_new.cpp
+++ src/asan_memory-leak_new.cpp
@@ -1,7 +1,10 @@
 // on macOS need to run with ASAN_OPTIONS=detect_leaks=true
 
+#include <iostream>
+
 int main()
 {
-    int* values = new int[10];
+    auto values = new int[10];
+    std::cout << "values[0] = " << values[0] << '\n';
     // delete[] values;
 }

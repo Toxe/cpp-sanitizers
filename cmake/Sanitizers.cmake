d547f729f9e8374098b27d8b77771cb589d23a11
Toxe <toxe@toxe.net>
2021-03-19 02:23:34 +0100

    GCC does not support memory sanitizer

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -1,3 +1,8 @@
+# unsupported sanitizers for GCC
+set(GCC_UNSUPPORTED_SANITIZERS
+    memory
+)
+
 option(USE_SANITIZER "Enable sanitizer. Options are: OFF, address, leak, thread, undefined, memory" OFF)
 
 string(TOLOWER ${USE_SANITIZER} USE_SANITIZER)
@@ -12,6 +17,12 @@ elseif(USE_SANITIZER STREQUAL "undefined")
     set(SANITIZER_FLAGS -fsanitize=undefined)
 elseif(USE_SANITIZER STREQUAL "memory")
     set(SANITIZER_FLAGS -fsanitize=memory -fsanitize-memory-track-origins -fno-omit-frame-pointer -fPIE -pie)
+endif()
+
+if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    if(USE_SANITIZER IN_LIST GCC_UNSUPPORTED_SANITIZERS)
+        message(SEND_ERROR "GCC does not support sanitizer: ${USE_SANITIZER}")
+    endif()
 endif()
 
 if(SANITIZER_FLAGS)

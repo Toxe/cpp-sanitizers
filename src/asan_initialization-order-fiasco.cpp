6803373e38145bd88fa5e9cfde186cb105298c04
Toxe <toxe@toxe.net>
2022-11-28 11:21:28 +0100

    Add compiler-specific noinline attributes

--- src/asan_initialization-order-fiasco.cpp
+++ src/asan_initialization-order-fiasco.cpp
@@ -4,7 +4,11 @@
 
 extern int global;
 
+#if defined(__GNUC__) || defined(__clang_version__)
 int __attribute__((noinline)) read_global()
+#elif defined(_MSC_VER)
+int __declspec(noinline) read_global()
+#endif
 {
     return global;
 }
